422d60029c60949fe6cbcac7c135405e7bee5340
fauzanpr <fauzanpr.06@gmail.com>
2023-07-20 19:09:23 +0700

    handling query error

--- src/components/RQSuperheroes.page.jsx
+++ src/components/RQSuperheroes.page.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { useQuery } from "react-query";
-import { Link } from "react-router-dom";
 
 const RQSuperHeroes = () => {
-  const { data, isLoading } = useQuery("superheroes", () => {
+  const { data, isLoading, isError, error } = useQuery("superheroes", () => {
     return axios.get("http://localhost:8000/superheroes");
   });
 
   if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>{ error.message }</p>
 
   return (
     <>
